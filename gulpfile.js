5666d93d1507f9b60841b40568d4a999bea36f6e
Gustavo Santos <gustavosantos@gustavosnto.local>
2023-03-29 00:09:42 -0300

    add new

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,14 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const browserSync = require('browser-sync').create();
 const tailwindcss = require('tailwindcss');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
 
 const PROJECT_DIR = __dirname;
 
 gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src('assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
       tailwindcss({
@@ -18,18 +21,51 @@ gulp.task('sass', function () {
       autoprefixer(),
       cssnano()
     ]))
-    .pipe(gulp.dest('src/css'))
+    .pipe(gulp.dest('assets/css'))
     .pipe(browserSync.stream());
 });
 
+function pluginsCSS() {
+  return gulp.src('./assets/css/libs/*.css')
+  .pipe(concat('plugins.css'))
+  .pipe(gulp.dest('./assets/css/'))
+  .pipe(browserSync.stream())
+}
+gulp.task('plugincss', pluginsCSS);
+
+function gulpJs() {
+  return gulp.src('./assets/js/scripts/*.js')
+  .pipe(concat('all.js'))
+  .pipe(babel({
+      presets: ['@babel/env']
+  }))
+  .pipe(uglify())
+  .pipe(gulp.dest('./assets/js/'))
+  .pipe(browserSync.stream());
+}
+gulp.task('alljs', gulpJs);
+
+function pluginsJs() {
+  return gulp
+  .src(['./assets/js/libs/*'])
+  .pipe(concat('plugins.js'))
+  .pipe(gulp.dest('./assets/js/'))
+  .pipe(browserSync.stream())
+}
+gulp.task('pluginjs', pluginsJs);
+
 gulp.task('serve', function () {
     browserSync.init({
-      proxy: 'http://aquiegamer.local',
-      open: false
+      server: {
+        baseDir: "./"
+      }
     });
   
-    gulp.watch('src/scss/**/*.scss', gulp.series('sass'));
-    gulp.watch('**/*.php').on('change', gulp.series('sass', browserSync.reload));
+    gulp.watch('./assets/scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('./assets/css/libs/*.css', pluginsCSS);
+    gulp.watch('**/*.html').on('change', gulp.series('sass', browserSync.reload));
+    gulp.watch('./assets/js/scripts/*js', gulpJs);
+    gulp.watch('./assets/js/libs/*.js', pluginsJs);
   });  
 
-gulp.task('default', gulp.series('sass', 'serve'));
+gulp.task('default', gulp.series('sass', 'plugincss', 'serve', 'alljs', 'pluginjs'));
